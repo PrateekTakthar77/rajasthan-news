4763c4459f9de7306bc5b9d490909005303a923f
Prateek Takthar <prateektakthar184b@gmail.com>
2023-11-29 13:10:46 +0530

    29 nov WINDOWS update

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -23,27 +23,27 @@ import ElectionsSubcategory from '../pages/Elections/ElectionsSubcategory';
 import SportsSubcategory from '../pages/Sports/SportsSubcategory';
 import EducationSubcategory from '../pages/Education/EducationSubcategory';
 import Health from '../pages/Health/Health';
-import Articles from '../pages/Articles/Articles';
+// import Articles from '../pages/Articles/Articles';
 
 function Navigation() {
     return (
         <BrowserRouter>
-           <Header/>
+            <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/:param1?/:param2?/:param3?' element={<FullNews />} />
-                <Route path='/articles/:id' element={<Articles />} />
+                {/* <Route path='/articles/:id' element={<Articles />} /> */}
                 <Route path='/election' element={<Elections />} />
                 {/* <Route path='/elections/:param1?/:param2?' element={<ElectionsSubcategory />} /> */}
                 <Route path='/election/:param1?' element={<ElectionsSubcategory />} />
                 <Route path='/sports' element={<Sports />} />
-                <Route path='/game/:param1?/:param2?' element={<SportsSubcategory/>} />
+                <Route path='/game/:param1?/:param2?' element={<SportsSubcategory />} />
                 <Route path='/education' element={<Education />} />
                 {/* <Route path='/education/:param1?/:param2?' element={<EducationSubcategory/>} /> */}
                 <Route path='/viralnews' element={<Viralnews />} />
-                <Route path='/technology' element={<Technology/>} />
-                <Route path='/entertainment' element={<Entertainment/>} />
-                <Route path='/health' element={<Health/>} />
+                <Route path='/technology' element={<Technology />} />
+                <Route path='/entertainment' element={<Entertainment />} />
+                <Route path='/health' element={<Health />} />
 
                 {/* <Route path='/all' element={<All />} /> */}
                 <Route path='/all-news' element={<All />} />
@@ -53,9 +53,9 @@ function Navigation() {
                 <Route path='/congress-news' element={<CongressNews />} />
                 {/* <Route path='/sportsnews' element={<SportsNews />} /> */}
                 <Route path='/sports-news' element={<SportsNews />} />
-                <Route path='/jdunews' element={<JduNews/>} />
+                <Route path='/jdunews' element={<JduNews />} />
                 <Route path='/breakingnews' element={<BreakingNews />} />
-                <Route path='/others' element={<Others/>} />
+                <Route path='/others' element={<Others />} />
             </Routes>
         </BrowserRouter>
     )
