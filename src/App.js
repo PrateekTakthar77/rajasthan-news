3fc2dd166e32488df7ab38a7e77af1636516cc21
Abhishek101januskoncepts <abhishek@januskoncepts.net>
2023-12-19 15:33:04 +0530

    19 dec 1st update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Box from '@mui/material/Box';
 import Navigation from './navigation/Navigation';
 import Footer from './components/Footer';
 import { Provider } from 'react-redux';
-import store from "./Redux/store"
+// import store from "./store"
+import store from './Redux/store';
 import { ThreeSixty } from '@mui/icons-material';
 
 function App() {
